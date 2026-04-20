d408eba0a48bdf228cb126d04e089b3ef6576321
bulutdevs <bulutdevelops@gmail.com>
2026-04-20 22:06:23 +0300

    feat(core): configure application routing structure

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Events from "./pages/Events";
+import Internship from "./pages/Internship";
+import Members from "./pages/Members";
+import Contact from "./pages/Contact";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="events" element={<Events />} />
+          <Route path="internship" element={<Internship />} />
+          <Route path="members" element={<Members />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
