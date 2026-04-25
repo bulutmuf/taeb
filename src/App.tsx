fdd9cf1eb06869905a5638d7e0130a59ee4e68a4
bulutdevs <bulutdevelops@gmail.com>
2026-04-25 16:11:30 +0300

    fix: make suspension full-screen and create thicker moving progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -16,35 +16,21 @@ const Membership = lazy(() => import("./pages/Membership"));
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route 
-            index 
-            element={
-              <Suspense fallback={<LoadingOverlay />}>
-                <Home />
-              </Suspense>
-            } 
-          />
-          <Route 
-            path="*" 
-            element={
-              <Suspense fallback={<LoadingOverlay />}>
-                <Routes>
-                  <Route path="about" element={<About />} />
-                  <Route path="projects" element={<Projects />} />
-                  <Route path="events" element={<Events />} />
-                  <Route path="internship" element={<Internship />} />
-                  <Route path="members" element={<Members />} />
-                  <Route path="contact" element={<Contact />} />
-                  <Route path="media" element={<Media />} />
-                  <Route path="membership" element={<Membership />} />
-                </Routes>
-              </Suspense>
-            } 
-          />
-        </Route>
-      </Routes>
+      <Suspense fallback={<LoadingOverlay />}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="events" element={<Events />} />
+            <Route path="internship" element={<Internship />} />
+            <Route path="members" element={<Members />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="media" element={<Media />} />
+            <Route path="membership" element={<Membership />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
