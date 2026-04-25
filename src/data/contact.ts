d09500144b015253cf6d2871c1eda0cbb7111556
bulutdevs <bulutdevelops@gmail.com>
2026-04-25 11:58:06 +0300

    feat: add social media links to footer

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,10 +2,20 @@ export interface ContactInfo {
   phone: string;
   email: string;
   address: string;
+  social: {
+    linkedin: string;
+    instagram: string;
+    twitter: string;
+  };
 }
 
 export const contactInfo: ContactInfo = {
   phone: "+1 (908) 313-8133",
   email: "support@taeb.us",
   address: "4383 Route 27 Princeton, NJ, USA",
+  social: {
+    linkedin: "https://www.linkedin.com/company/turkish-american-pharmacists-association-taeb/",
+    instagram: "https://www.instagram.com/taeb.usa",
+    twitter: "https://www.twitter.com/taeb_official"
+  }
 };
