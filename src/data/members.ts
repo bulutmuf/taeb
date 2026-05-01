26aef692a99d37c9f9b0776e94fd6fd9572f1ec1
bulutdevs <bulutdevelops@gmail.com>
2026-05-02 00:15:47 +0300

    fix: align dev team member full name to Ulku Irem Keskin for shared profile photo resolution

--- src/data/members.ts
+++ src/data/members.ts
@@ -48,7 +48,7 @@ export const developmentTeam: BoardMember[] = [
     image: "" 
   },
   { 
-    name: _sc([309, 119, 106, 114, 37, 80, 106, 120, 112, 110, 115]), 
+    name: _sc([225, 113, 112, 257, 37, 309, 119, 106, 114, 37, 80, 106, 120, 112, 110, 115]), 
     roleEn: "Developer", 
     roleTr: "Geliştirici", 
     image: "" 
